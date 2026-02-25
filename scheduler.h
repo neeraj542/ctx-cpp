13c28f80c29a63e4855ea75814744302cca65dbc
Neeraj Meena 🌱 <114648043+neeraj542@users.noreply.github.com>
2026-02-26 00:27:03 +0530

    Update scheduler.h

--- scheduler.h
+++ scheduler.h
@@ -1,1 +1,25 @@
 
+#ifndef SCHEDULER_H
+#define SCHEDULER_H
+
+#include <vector>
+#include <pthread.h>
+#include "task_queue.h"
+
+class Scheduler {
+private:
+    int thread_count;
+    std::vector<pthread_t> workers;
+    TaskQueue taskQueue;
+
+    static void* worker(void* arg);
+
+public:
+    Scheduler(int threads);
+    ~Scheduler();
+
+    void submit(Task task);
+    void shutdown();
+};
+
+#endif
