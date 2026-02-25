011937939405aa6ee8220a51665caa5f696cf392
Neeraj Meena 🌱 <114648043+neeraj542@users.noreply.github.com>
2026-02-26 00:25:47 +0530

    Update task.h

--- task.h
+++ task.h
@@ -1,1 +1,16 @@
 
+#ifndef TASK_H
+#define TASK_H
+
+#include <functional>
+
+struct Task {
+    int id;
+    int execution_time; // milliseconds
+    int priority;
+
+    Task(int id_, int exec_time_, int priority_)
+        : id(id_), execution_time(exec_time_), priority(priority_) {}
+};
+
+#endif
