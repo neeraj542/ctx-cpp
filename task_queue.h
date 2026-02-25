fb14d1f74d091d350c0fb9a08762890be7c9e7ce
Neeraj Meena 🌱 <114648043+neeraj542@users.noreply.github.com>
2026-02-26 00:26:16 +0530

    Update task_queue.h

--- task_queue.h
+++ task_queue.h
@@ -1,1 +1,25 @@
 
+#ifndef TASK_QUEUE_H
+#define TASK_QUEUE_H
+
+#include <queue>
+#include <pthread.h>
+#include "task.h"
+
+class TaskQueue {
+private:
+    std::queue<Task> queue;
+    pthread_mutex_t mutex;
+    pthread_cond_t cond;
+    bool shutdown_flag;
+
+public:
+    TaskQueue();
+    ~TaskQueue();
+
+    void push(Task task);
+    bool pop(Task &task);
+    void shutdown();
+};
+
+#endif
