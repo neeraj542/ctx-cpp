894f60235dac28dbbf5d65bed5eb06adf58a023d
Neeraj Meena 🌱 <114648043+neeraj542@users.noreply.github.com>
2026-02-26 00:27:54 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,16 @@
 
+#include "scheduler.h"
+#include <iostream>
+
+int main() {
+    Scheduler scheduler(4);
+
+    for (int i = 1; i <= 10; i++) {
+        scheduler.submit(Task(i, 500, 1));
+    }
+
+    sleep(3);
+
+    scheduler.shutdown();
+    return 0;
+}
